4cb5ccdee588e6d5d6329923147d2bc204083d1b
Ramiro1993-test <57078972+Ramiro1993-test@users.noreply.github.com>
2022-07-12 19:13:03 -0500

    Create modulo-main.py

---
+++ modulo-main.py
@@ -0,0 +1,9 @@
+import operacion
+
+def main ():
+  print ('la suma es',operacion.sumar(5,4))
+  print ('la resta es',operacion.restar(10,4))
+  print ('El producto es ',operacion.multiplicar(6,4))
+  print ('la division es ',operacion.dividir(45,5))
+   
+if __name__ == '__main__':
