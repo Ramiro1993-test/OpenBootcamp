2c441510d5a91f39cc91536176a9457869dba703
Ramiro1993-test <57078972+Ramiro1993-test@users.noreply.github.com>
2022-07-15 13:39:01 -0500

    Update e-db.py

--- e-db.py
+++ e-db.py
@@ -1,17 +1,25 @@
-def crear_usuario(identificador,nombre,apellido):
- n=int(input("Ingrese cantidad de alumnos a registrar: "))
- for x in range(n):
-    identificador=int(input("Ingrese identicador: "))
-    nombre=input("Ingrese nombre: ")
-    apellido=input("Ingrese nombre: ")
-    return (identificador, nombre, apellido)
- conn=sqlite3.connect('maplicacion.db')
- cursor=conn.cursor()
- 
- query='INSERT INTO users(id,nombre,apellido)VALUES(?,?,?)'
+import sqlite3
 
- cursor.execute(query,(identificador,nombre,apellido))
- identificador+=1
- conn.commit()
- cursor.close()
- conn.close()
+db_connection = sqlite3.connect('miaplicacion.db')
+db_cursor = db_connection.cursor()
+
+db_cursor.execute("CREATE TABLE empleados(Id INT, Nombre TEXT, Apellido TEXT)")
+
+db_cursor.execute("INSERT INTO Alumnos VALUES(1,'Maria', 'Lopez')")
+db_cursor.execute("INSERT INTO Alumnos VALUES(2,'Pepe', 'Rodriguez')")
+db_cursor.execute("INSERT INTO Alumnos VALUES(3,'Maria', 'Martinez')")
+db_cursor.execute("INSERT INTO Alumnos VALUES(4,'Francisco', 'Perez')")
+db_cursor.execute("INSERT INTO Alumnos VALUES(5,'Jorge', 'Herrero')")
+db_cursor.execute("INSERT INTO Alumnos VALUES(6,'Clara', 'García')")
+db_cursor.execute("INSERT INTO Alumnos VALUES(7,'Monica', 'Santamaria')")
+db_cursor.execute("INSERT INTO Alumnos VALUES(8,'Pablo', 'Villanueva')")
+
+db_connection.commit()
+
+db_cursor.execute("SELECT * FROM Alumnos WHERE Nombre = 'Maria'")
+
+filas = db_cursor.fetchall()
+
+print(filas)
+
+db_connection.close()
